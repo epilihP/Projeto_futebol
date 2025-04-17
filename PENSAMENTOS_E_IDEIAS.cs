8441dfe2db8602bbea42016bfb97cbce1fdb6a44
epilihP <philipegcustodio@gmail.com>
2025-04-17 19:56:39 -0300

    orgabizando ideias e colocando a os atributos ala, fixo e goleiro na classses goleiro

---
+++ PENSAMENTOS_E_IDEIAS.cs
@@ -0,0 +1,28 @@
+// =-=-=-=-=-=-=-=Ideias=-=-=-=-=-=-=-=
+
+
+
+//  Usar uma biblioteca random pra sortear as pontuações da vitória
+
+// Fazer um futsal, menos jogadores e mais fácil de organizar tendo 2 alas, 2 fixos e goleiro
+
+// Timeout
+
+// Classe principal de gerenciamento de time
+
+// Classe principal (pt2) para o gerenciamento de pessoas (associação)
+
+// C/ Herança vai tar as classes
+
+// Jogador (atributos:
+// Ala,
+// Fixo,
+// Goleiro)
+// Partida (atributos:
+//  time1,
+// time2,
+// time3,
+// balancear partidas: métodos
+// {
+//    2 ala para cada time, 2 fixos para cada            time, goleiro
+// }
---
+++ SonModels/Jogador.cs
@@ -0,0 +1,14 @@
+using DadModels;
+
+namespace DadModels;
+
+namespace SonModels;
+
+public class Jogador : Associacao
+{
+    private string Fixo { get; set; }
+
+    private string Ala { get; set; }
+
+    private string Goleiro { get; set; }
+}
