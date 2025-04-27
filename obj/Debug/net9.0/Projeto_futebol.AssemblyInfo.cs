390e33c8c1dcfda9924009550af92a9c9e79c9ab
alissonarsa <alissonarsa@gmail.com>
2025-04-27 12:43:12 -0300

    mais estruturação do diretorio

--- obj/Debug/net9.0/Projeto_futebol.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projeto_futebol.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_futebol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d3a5e2b867f89381f95626486fb423ba0286925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f01bb5b305da97c65ca4b757c29692fcf20ae5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_futebol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_futebol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
