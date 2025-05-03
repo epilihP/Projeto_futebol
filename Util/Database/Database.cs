49745d32b65e4716ba211b328f1b78703460b99a
alissonarsa <alissonarsa@gmail.com>
2025-05-03 18:01:41 -0300

    implementado método CRUD no jogador

--- Util/Database/Database.cs
+++ Util/Database/Database.cs
@@ -3,7 +3,7 @@ using Players;
 
 public class Database
 {
-    private static readonly string FilePath = @"c:\Users\aliss\Documents\Faculdade\Programação Orientada a Objetos\Projeto Futebol\Projeto_futebol\Util\Database\associados.json";
+    internal static readonly string FilePath = @"c:\Users\aliss\Documents\Faculdade\Programação Orientada a Objetos\Projeto Futebol\Projeto_futebol\Util\Database\associados.json";
 
     public static void SalvarJogador(Jogador jogador)
     {
@@ -28,5 +28,10 @@ public class Database
         string novoJson = JsonSerializer.Serialize(jogadores, new JsonSerializerOptions { WriteIndented = true });
 
         File.WriteAllText(FilePath, novoJson); // pra salvar no aqrquivo
+    }
+    public static void AtualizarArquivo(List<Jogador> jogadores)
+    {
+        string novoJson = JsonSerializer.Serialize(jogadores, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(FilePath, novoJson);
     }
 }
